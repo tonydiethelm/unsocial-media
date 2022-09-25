f07a37ef5478398eb0e3ec8cb2cc471f83eee30a
Tony Diethelm <tonydiethelm@gmail.com>
2022-09-24 20:51:17 -0700

    Further documentation

--- mapAFolder.js
+++ mapAFolder.js
@@ -1,6 +1,36 @@
-/* This maps a given directory and returns an object of objects describing it. 
-One object will hold the full directory path. 
+/* I need to see what's in a directory.
+I need to get a list of folders
+I need to get a list of pictures. 
+I need to get a list of text files. 
+
+https://nodejs.org/api/fs.html#synchronous-example
+
+asynchronous
+fs.readdir()        returns an array containing an array containing file names in the directory you've specified. 
+
+fsPromises.readdir(path[, options])
 
 
+synchronous
+fs.readdirSync()    
+
+
+
+
+fsPromises.stat(path[, options])
+Can get stats to see what is a directory and what is a file. 
+fs.fstat(fd[, options], callback)
 
 */
+
+
+const testDir = '~/Codesmith';
+fs
+
+
+
+
+// For testing each module
+if (require.main !== module) {
+
+}
