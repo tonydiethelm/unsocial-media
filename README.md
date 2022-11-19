2c448b06d383715c7918ed06cf207cc4ed356c01
Tony Diethelm <tonydiethelm@gmail.com>
2022-11-19 09:05:18 -0800

    Starting rework of Unsocial Media

---
+++ README.md
@@ -0,0 +1,18 @@
+#Unsocial Media
+Unsocial Media is a small web server that creates content based on the contents of a folder in your hard drive. Put in Text, Pictures, Files and subfolders and it will make a page. 
+
+This means non technical people can easily* put up a web server to share Stuff with the world. 
+
+(If hosting at home, one would still have to port forward from their home router.)
+
+##Install
+
+##Use
+
+##Settings
+
+##Written By
+Tony Diethelm | [github](https://github.com/tonydiethelm) | [linked](https://www.linkedin.com/in/tonydiethelm)
+
+######Miro Board
+https://miro.com/app/board/uXjVPCLb2A8=/
---
+++ express/express.mjs
@@ -0,0 +1,73 @@
+/* What this needs to do:
+I need to static serve my HTML, JS, CSS.
+I need to static serve imeges from the target directory (see README)
+I need to static serve a settings page.
+
+I need to respond to GETs with parameters to /api/directory
+
+I need to handle wrong URIs
+I need to handle errors
+Actually call the HTTP server.
+
+*/
+
+
+
+//set up everything
+
+//fs for pathnames
+import * as fs from 'fs';
+
+//get settings from settings.json
+import { settings } from './settings.mjs';
+
+//express set up
+import express from 'express';
+const app = express();
+
+//automatically destring incoming JSON
+app.use(express.json());
+
+//import my middleware/controllers
+//ToDo
+
+//extra stuff
+import holler from '@tonydiethelm/holler';
+
+
+
+//Express routes and static serves
+
+//static serves
+//HTML, CSS, JS, settings.html
+//get file path URL object
+const publicDirectory = new URL('public', import.meta.url); 
+app.get(express.static(publicDirectory.pathname));
+//serve up pictures
+app.get('/api/assets', express.static(settings.targetDirectory))
+
+
+//handle requests to directory api
+app.get('/api/directory:targetDirectory',
+holler,
+(request, response) => {response.status(200).send('In progress')}
+//(request, response) => {response.status(200).send(response.locals.mapping)}
+);
+
+
+
+
+//handle wrong URIs
+app.use('*', 
+  (request, response) => {
+    response.status(404).json('Sorry, we don\'t have that here.')
+});
+
+//handle errors
+//ToDo
+
+
+//Call the HTTP server 
+app.listen(port, () => {
+  console.log('Server listening on port: ' + settings.port);
+ });
